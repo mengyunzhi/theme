283c49aedbfb22bef57d19b49c590bd2a00406f0
panjie <3792535@qq.com>
2019-05-17 16:29:09 +0800

    导航初始化

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
