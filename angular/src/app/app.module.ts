963397cd4ee30f921ab85cbfb1e07f46584a7d41
panjie <3792535@qq.com>
2019-05-17 16:14:09 +0800

    修改前缀，增加footer

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
