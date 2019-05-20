b5f0723f2470bedeba9ca11a8be7211ae6721904
zhangxishuo <zhangxishuo1998@gmail.com>
2019-05-20 11:03:02 +0800

    初始化菜单结构

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,22 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavigationComponent } from './navigation/navigation.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FooterComponent} from './footer/footer.component';
+import {NavigationComponent} from './navigation/navigation.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent,
-    NavigationComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FooterComponent,
+        NavigationComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- angular/src/app/navigation/navigation.component.ts
+++ angular/src/app/navigation/navigation.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {Menu} from '../target/menu';
 
 @Component({
     selector: 'yz-navigation',
@@ -7,10 +9,32 @@ import {Component, OnInit} from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-    constructor() {
+    menus: Array<Menu>;
+
+    constructor(private router: Router) {
+        this.menus = [];
+
+        // Dashboard
+        this.menus.push(new Menu('Dashboard', 'dashboard', [], '<span class="label label-success pull-right">V.1</span>'));
+
+        // Analytics
+        this.menus.push(new Menu('Analytics', 'analytics', [], '<span class="label label-warning pull-right">NEW</span>'));
+
+        // Interface
+        const interfaces = [];
+        interfaces.push(new Menu('Panels design', 'interface/panels'));
+        interfaces.push(new Menu('Typography', 'interface/typography'));
+        interfaces.push(new Menu('Colors & Buttons', 'interface/buttons'));
+        this.menus.push(new Menu('Interface', '', interfaces));
     }
 
     ngOnInit() {
     }
 
+    /**
+     * 路由导航
+     * @param menu 菜单
+     */
+    navigate(menu: Menu) {
+    }
 }
